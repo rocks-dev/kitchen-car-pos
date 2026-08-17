2eedccafcc1f6fc635cd6039d9588b828f3a1099
rocks-dev <yuiichi.iwanaga@gmail.com>
2026-08-17 09:49:51 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kitchen-car-pos-pwa-v5-1';
+const CACHE='kitchen-car-pos-pwa-v5-3-3-2';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
